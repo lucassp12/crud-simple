207a9c3d0555b21ef935139a409c2ad69a5cea8c
Lenon Mauer <lenonmauer@gmail.com>
2019-04-06 14:00:37 -0300

    removido codigo inutil

--- server.js
+++ server.js
@@ -1,31 +1,31 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const requireDir = require("require-dir");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const expressLayouts = require("express-ejs-layouts");
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const expressLayouts = require('express-ejs-layouts');
 
 // iniciando app
 const app = express();
+const port = 3001;
+
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //iniciando o DB
-mongoose.connect("mongodb://localhost:27017/crud-simple", {
-  useNewUrlParser: true
+mongoose.connect('mongodb://localhost:27017/crud-simple', {
+  useNewUrlParser: true,
 });
 
-requireDir("./src/model");
-
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
 
 // view engine setup
-app.set("views", __dirname + "/src/views");
-app.set("view engine", "ejs");
+app.set('views', __dirname + '/src/views');
+app.set('view engine', 'ejs');
 app.use(expressLayouts);
-app.use(bodyParser.urlencoded());
 
 //rotas
-app.use("/", require("./src/routes"));
+app.use('/', require('./src/routes'));
 
-app.listen(3001);
+app.listen(port, () => {
+  console.log(`Listening at ${port}`);
+});
